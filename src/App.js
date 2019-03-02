5b91081417d0ba5c2f3fe69b8be7d38ff09b02e9
c33k <rltoscano@gmail.com>
2019-03-02 18:53:53 +0100

    Create SideBar component

--- src/App.js
+++ src/App.js
@@ -1,55 +1,17 @@
 import React, { Component } from 'react';
-import { Layout, Menu, Icon } from 'antd';
+import { Layout } from 'antd';
+import SideBar from './SideBar/SideBar';
 import './App.css';
 
-const { Header, Content, Footer, Sider } = Layout;
+const { Header, Content, Footer } = Layout;
 
 class App extends Component {
-  state = {
-    menuCollapsed: false
-  };
-
-  onCollapse = collapsed => {
-    this.setState({ collapsed });
-  };
-
   //TODO: break into smaller components
 
   render() {
     return (
       <Layout style={{ minHeight: '100vh' }}>
-        <Sider
-          collapsible
-          collapsed={this.state.collapsed}
-          onCollapse={this.onCollapse}
-        >
-          <Menu theme='dark' defaultSelectedKeys={['1']} mode='inline'>
-            <Menu.Item to='/' key='0' style={{ paddingLeft: 'none' }}>
-              <img
-                className='ant-menu-item'
-                style={{ width: '50%', margin: 'auto' }}
-                src='logo2.png'
-                alt='Nogin Media Logo'
-              />
-            </Menu.Item>
-            <Menu.Item key='1'>
-              <Icon type='home' />
-              <span>Home</span>
-            </Menu.Item>
-            <Menu.Item key='2'>
-              <Icon type='user' />
-              <span>Profile</span>
-            </Menu.Item>
-            <Menu.Item key='3'>
-              <Icon type='bars' />
-              <span>EPG</span>
-            </Menu.Item>
-            <Menu.Item key='4'>
-              <Icon type='search' />
-              <span>Search</span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
+        <SideBar />
         <Layout>
           <Header className='app-header' />
           <Content style={{ margin: '0 16px' }}>
@@ -58,7 +20,7 @@ class App extends Component {
                 padding: 24,
                 background: '#202020',
                 minHeight: 360,
-                color: "#eee"
+                color: '#eee'
               }}
             >
               random content to test colors
