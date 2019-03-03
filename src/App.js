3b4d8d52bee8015aa3e545bcffec09300b31d757
c33k <rltoscano@gmail.com>
2019-03-03 11:04:00 +0100

    Update components to use hooks state hooks and to use functional approach

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Layout } from 'antd';
 import SideBar from './SideBar/SideBar';
 import './App.css';
 
 const { Header, Content, Footer } = Layout;
 
-class App extends Component {
-  //TODO: break into smaller components
-
-  render() {
-    return (
-      <Layout style={{ minHeight: '100vh' }}>
-        <SideBar />
-        <Layout>
-          <Header className='app-header' />
-          <Content style={{ margin: '0 16px' }}>
-            <div
-              style={{
-                padding: 24,
-                background: '#202020',
-                minHeight: 360,
-                color: '#eee'
-              }}
-            >
-              random content to test colors
-            </div>
-          </Content>
-          <Footer
+export default function App(props) {
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <SideBar />
+      <Layout>
+        <Header className='app-header' />
+        <Content style={{ margin: '0 16px' }}>
+          <div
             style={{
-              textAlign: 'center',
+              padding: 24,
               background: '#202020',
+              minHeight: 360,
               color: '#eee'
             }}
           >
-            Norigin Media ©2019 Created by RLToscano
-          </Footer>
-        </Layout>
+            random content to test colors
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+            background: '#202020',
+            color: '#eee'
+          }}
+        >
+          Norigin Media ©2019 Created by RLToscano
+        </Footer>
       </Layout>
-    );
-  }
-}
-
-export default App;
+    </Layout>
+  );
+};
--- src/SideBar/SideBar.js
+++ src/SideBar/SideBar.js
@@ -1,57 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Layout, Menu, Icon } from 'antd';
 import './SideBar.css';
 
 const { Sider } = Layout;
 
-class SideBar extends Component {  
-  state = {
-    menuCollapsed: false
-  };
+export default function SideBar(props) {
+  const [collapsed, setCollapsed] = useState(false);
+  const classLogoImg = `menu-item-logo-img ${collapsed ? ' collapsed' : ''}`;
+  const handleOnCollapse = collapsed => setCollapsed(collapsed);
 
-  onCollapse = collapsed => {
-    this.setState({ collapsed });
-  };
-
-  render() {
-    const { collapsed } = this.state;
-    const classLogoImg = `menu-item-logo-img ${collapsed ? ' collapsed' : ''}`;
-
-    return (
-      <Sider
-        className='sidebar'
-        collapsible
-        collapsed={collapsed}
-        onCollapse={this.onCollapse}
-      >
-        <Menu theme='dark' defaultSelectedKeys={['1']} mode='inline'>
-          <Menu.Item to='/' key='0' className='menu-item-logo'>
-            <img
-              className={classLogoImg}
-              src='logo.png'
-              alt='Nogin Media Logo'
-            />
-          </Menu.Item>
-          <Menu.Item key='1'>
-            <Icon type='home' />
-            <span>Home</span>
-          </Menu.Item>
-          <Menu.Item key='2'>
-            <Icon type='user' />
-            <span>Profile</span>
-          </Menu.Item>
-          <Menu.Item key='3'>
-            <Icon type='bars' />
-            <span>EPG</span>
-          </Menu.Item>
-          <Menu.Item key='4'>
-            <Icon type='search' />
-            <span>Search</span>
-          </Menu.Item>
-        </Menu>
-      </Sider>
-    );
-  }
+  return (
+    <Sider
+      className='sidebar'
+      collapsible
+      collapsed={collapsed}
+      onCollapse={handleOnCollapse}
+    >
+      <Menu theme='dark' defaultSelectedKeys={['1']} mode='inline'>
+        <Menu.Item to='/' key='0' className='menu-item-logo'>
+          <img className={classLogoImg} src='logo.png' alt='Nogin Media Logo' />
+        </Menu.Item>
+        <Menu.Item key='1'>
+          <Icon type='home' />
+          <span>Home</span>
+        </Menu.Item>
+        <Menu.Item key='2'>
+          <Icon type='user' />
+          <span>Profile</span>
+        </Menu.Item>
+        <Menu.Item key='3'>
+          <Icon type='bars' />
+          <span>EPG</span>
+        </Menu.Item>
+        <Menu.Item key='4'>
+          <Icon type='search' />
+          <span>Search</span>
+        </Menu.Item>
+      </Menu>
+    </Sider>
+  );
 }
-
-export default SideBar;
